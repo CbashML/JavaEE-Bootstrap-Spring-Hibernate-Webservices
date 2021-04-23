fcb5792f8183f29abd0f5ca2bda88b2f2eb93deb
Sebastian M. L. <cbashml91@gmail.com>
2021-04-23 20:04:57 +0200

    POO - Cuadro &TamanhoLienzo

--- Java_2021/IPartek/src/com/ipartek/ejercicios/poo/entidades/cuadro/TamanhoLienzo.java
+++ Java_2021/IPartek/src/com/ipartek/ejercicios/poo/entidades/cuadro/TamanhoLienzo.java
@@ -1,5 +1,25 @@
 package com.ipartek.ejercicios.poo.entidades.cuadro;
 
 public class TamanhoLienzo {
-
+	
+	private float altura;
+	private float anchura;
+	
+	public TamanhoLienzo(float altura, float anchura) {
+		setAltura(altura);
+		setAnchura(anchura);
+	}
+	
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	public float getAnchura() {
+		return anchura;
+	}
+	public void setAnchura(float anchura) {
+		this.anchura = anchura;
+	}
 }
