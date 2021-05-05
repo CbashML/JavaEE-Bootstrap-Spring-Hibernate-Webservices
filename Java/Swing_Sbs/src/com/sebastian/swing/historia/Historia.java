afc32bf81a7480db8305cc41fec5aaa09d236059
Sebastian M. L. <cbashml91@gmail.com>
2021-05-05 19:31:50 +0200

    Historia, Descripcion - GUI (Swing)

--- Java/Swing_Sbs/src/com/sebastian/swing/historia/Historia.java
+++ Java/Swing_Sbs/src/com/sebastian/swing/historia/Historia.java
@@ -2,19 +2,16 @@ package com.sebastian.swing.historia;
 
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Font;
-import javax.swing.JTextPane;
 
 public class Historia {
 
